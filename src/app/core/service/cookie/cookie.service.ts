6672a16e737cc3ed859a8216c870a828e757f60c
SergioTrinquet <serge.trinquet@gmail.com>
2025-10-31 17:24:43 +0100

    cleaning code + declaration methodes en public

--- src/app/core/service/cookie/cookie.service.ts
+++ src/app/core/service/cookie/cookie.service.ts
@@ -29,20 +29,14 @@ export class CookieService {
     document.cookie = `${COOKIE_NAME_PREFIX}${this.selectState.selectedValueNbDots()}=;max-age=0; path=/`;
   });
 
-  setCookie(gridSize: number, combination: number[]): void {  
+  public setCookie(gridSize: number, combination: number[]): void {  
     const data = { gridSize, combination };
     const durationCookieInSec = 60*60*24*30;
     const cookie = `${COOKIE_NAME_PREFIX}${gridSize}=${JSON.stringify(data)}; max-age=${durationCookieInSec}; path=/`; // Voir aussi avec les parametres 'sameSite', 'lax', 'strict', 'secure',...
     document.cookie = cookie;
   }
 
-  /* getCookieValue(name: string): string | undefined {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift();
-    return undefined;
-  } */
- getCookieValue(name: string): string {
+  public getCookieValue(name: string): string {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop()?.split(';').shift() ?? "";
