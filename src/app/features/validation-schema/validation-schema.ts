da521582a259203e327c71d1ae3401f49198523f
SergioTrinquet <serge.trinquet@gmail.com>
2025-11-03 15:45:24 +0100

    Fin traitement sur actions boutons

--- src/app/features/validation-schema/validation-schema.ts
+++ src/app/features/validation-schema/validation-schema.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { TouchScreenService } from '../../shared/services/touch-screen/touch-screen.service';
-import { CookieService } from '../../core/service'
-import { AnimationService } from '../grid/services';
+import { AnimationService, GridStateService } from '../grid/services';
+import { SelectControlService, SelectStateService } from '../select/services';
+import { CookieService } from '../../core/service/cookie/cookie.service';
+import { ResetSchemaService } from './services/reset-schema/reset-schema.service';
 
 @Component({
   selector: 'app-validation-schema',
@@ -10,61 +12,38 @@ import { AnimationService } from '../grid/services';
   styleUrl: './validation-schema.scss'
 })
 export class ValidationSchema {
-  private TouchScreenService = inject(TouchScreenService);
-  // private cookieService = inject(CookieService);
+  private touchScreenService = inject(TouchScreenService);
+  private cookieService = inject(CookieService);
   private animationService = inject(AnimationService);
+  private resetSchemaService = inject(ResetSchemaService);
+  private selectStateService = inject(SelectStateService);
+  private selectControlService = inject(SelectControlService);
+  private gridStateService = inject(GridStateService);
 
-  protected onRedo(): void {
-    this.TouchScreenService.vibrateOnTouch(100);
-    // colorationSchema(true, STROKE.color.error);
+  private capturedDots = this.gridStateService.capturedDots;
+
+  protected onReset(): void {
+    this.touchScreenService.vibrateOnTouch(100);
+    this.resetSchemaService.triggerResetSchema(true);
     this.animationService.triggerContainerVibrateAnimation("timeVibrate")
       .subscribe({
-        complete: () => { 
-          console.log("Animation Vibrate terminée !!!!");
-          // removeSchemaDrawing();
-          // setComplementaryInfos();
-        }
+        complete: () => this.resetSchemaService.triggerResetSchema(false)
       });
-
-    /* 
-    colorationSchema(true, STROKE.color.error);
-    s.container.addEventListener("animationend", (e) => {
-        if (e.animationName === "vibrate") {
-            s.container.classList.remove("vibrate");
-            removeSchemaDrawing();
-            setComplementaryInfos();
-        }
-    }, { once: true }); */
   }
 
   protected onValidate() {
-    this.TouchScreenService.vibrateOnTouch([100, 50, 100]);
+    this.touchScreenService.vibrateOnTouch([100, 50, 100]);
     this.animationService.triggerGridAnimation("timePulse")
       .subscribe({
         complete: () => {
-          console.log("Animation Pulse terminée !!!!");
+          const nbDotsGrid = this.selectStateService.selectedValueNbDots();
+          if(nbDotsGrid) this.cookieService.setCookie(nbDotsGrid, this.capturedDots());
 
-          this.animationService.triggerGridAnimation("timeShrink")
+          this.animationService.triggerContainerShrinkAnimation("timeShrink")
             .subscribe({
-              complete: () => console.log("Animation Vibrate terminée !!!!")
+              complete: () => this.selectControlService.resetSelectToDefault()
             });
         }
       });
-
-    // this.cookieService.setCookie(this.state.selectedValueNbDots(), s.captureDots); // Création cookie
-
-    /* s.gridPoints.classList.add("pulse");
-    s.gridPoints.addEventListener("animationend", () => {
-        s.gridPoints.classList.remove("pulse");
-        s.container.classList.add("shrink");
-        this.cookieService.setCookie(this.state.selectedValueNbDots(), s.captureDots); // Création cookie
-    }, { once: true }); */
-
-    /* s.container.addEventListener("animationend", (e) => {
-        if (e.animationName === animationShrink) {
-            s.container.classList.remove("shrink");
-            goBackToStartStep();
-        }
-    }); */
   }
 }
