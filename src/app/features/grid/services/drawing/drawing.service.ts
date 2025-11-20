7b5daa53ff12046fdb1076ee942f7e9546c16ec0
SergioTrinquet <serge.trinquet@gmail.com>
2025-11-20 15:49:13 +0100

    chore: ajout declaration methodes en 'public'

--- src/app/features/grid/services/drawing/drawing.service.ts
+++ src/app/features/grid/services/drawing/drawing.service.ts
@@ -10,7 +10,7 @@ export class DrawingService {
   private canvas = inject(ResizeObserverService).canvas;
 
   // Peut-être à mettre dans un service à part
-  getCursorPositionOnCanvas(e: MouseEvent | PointerEvent): CoordStrokeExtremity {
+  public getCursorPositionOnCanvas(e: MouseEvent | PointerEvent): CoordStrokeExtremity {
     const { clientX, clientY } = e;
     // Return position from the origin of canvas
     return { 
@@ -20,11 +20,11 @@ export class DrawingService {
   }
 
 
-  refreshCanvas(ctx: CanvasRenderingContext2D): void {
+  public refreshCanvas(ctx: CanvasRenderingContext2D): void {
     ctx.clearRect(0, 0, this.canvas().width, this.canvas().height);
   }
 
-  draw(ctx: CanvasRenderingContext2D, coordStrokes: Stroke[], currentColor: string): void { /* 'currentColor' utile ? Parce que qd tracé, est tjs blanc! */
+  public draw(ctx: CanvasRenderingContext2D, coordStrokes: Stroke[], currentColor: string): void { /* 'currentColor' utile ? Parce que qd tracé, est tjs blanc! */
     // Boucle sur nb de traits
     for(const stroke of coordStrokes) {
       if(stroke.end.x && stroke.end.y) {
