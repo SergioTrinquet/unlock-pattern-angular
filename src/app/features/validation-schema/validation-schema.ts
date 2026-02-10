d3e66befd8e2661e540a73d75fdf327a3fde798f
SergioTrinquet <serge.trinquet@gmail.com>
2026-02-10 10:32:26 +0100

    chore: chgmt nom services appelés + stop animation traits sur action bt 'Refaire le schéma'

--- src/app/features/validation-schema/validation-schema.ts
+++ src/app/features/validation-schema/validation-schema.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { TouchScreenService } from '../../shared/services/touch-screen/touch-screen.service';
-import { AnimationService, GridStateService } from '../grid/services';
+import { AnimationBackgroundGridService, GridStateService } from '../grid/services';
 import { SelectControlService, SelectStateService } from '../select/services';
 import { CookieService } from '../../core/service/cookie/cookie.service';
 import { ResetSchemaService } from './services/reset-schema/reset-schema.service';
+import { AbortStrokesAnimationService } from '../../shared/services/abort-strokes-animation/abort-strokes-animation.service';
 
 @Component({
   selector: 'app-validation-schema',
@@ -15,18 +16,21 @@ import { ResetSchemaService } from './services/reset-schema/reset-schema.service
 export class ValidationSchema {
   private touchScreenService = inject(TouchScreenService);
   private cookieService = inject(CookieService);
-  private animationService = inject(AnimationService);
+  private animationBackgroundGridService = inject(AnimationBackgroundGridService);
   private resetSchemaService = inject(ResetSchemaService);
   private selectStateService = inject(SelectStateService);
   private selectControlService = inject(SelectControlService);
   private gridStateService = inject(GridStateService);
+  private abortStrokesAnimationService = inject(AbortStrokesAnimationService);// TEST
 
   private capturedDots = this.gridStateService.capturedDots;
 
+
   protected onReset(): void {
     this.touchScreenService.vibrateOnTouch(100);
+    this.abortStrokesAnimationService.stopSequence();// Pour arrêter toute animation en cours avant de réinitialiser le schéma (ex: si l'utilisateur clique sur 'Reset' pendant que les traits du schéma sont en train de se colorer)
     this.resetSchemaService.triggerResetSchema(true);
-    this.animationService.triggerContainerVibrateAnimation("timeVibrate")
+    this.animationBackgroundGridService.triggerContainerVibrateAnimation("timeVibrate")
       .subscribe({
         complete: () => this.resetSchemaService.triggerResetSchema(false)
       });
@@ -34,13 +38,13 @@ export class ValidationSchema {
 
   protected onValidate(): void {
     this.touchScreenService.vibrateOnTouch([100, 50, 100]);
-    this.animationService.triggerGridAnimation("timePulse")
+    this.animationBackgroundGridService.triggerGridAnimation("timePulse")
       .subscribe({
         complete: () => {
           const nbDotsGrid = this.selectStateService.selectedValueNbDots();
           if(nbDotsGrid) this.cookieService.setCookie(nbDotsGrid, this.capturedDots());
 
-          this.animationService.triggerContainerShrinkAnimation("timeShrink")
+          this.animationBackgroundGridService.triggerContainerShrinkAnimation("timeShrink")
             .subscribe({
               complete: () => this.selectControlService.resetSelectToDefault()
             });
