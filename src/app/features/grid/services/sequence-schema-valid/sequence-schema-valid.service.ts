e50f36bcd15ebd0921f0ee42ed52f19d7b33c116
SergioTrinquet <serge.trinquet@gmail.com>
2026-02-17 17:41:18 +0100

    chore: retrait commentaires suite à test pour ecran msg qd succès

--- src/app/features/grid/services/sequence-schema-valid/sequence-schema-valid.service.ts
+++ src/app/features/grid/services/sequence-schema-valid/sequence-schema-valid.service.ts
@@ -50,7 +50,7 @@ export class SequenceSchemaValidService {
         this.setSignal(this._growCardAfterFlipOver, false);
       }),
       delay(stepDelayBeforeMsgSuccessClose),
-      // tap(() => this.screenDisappearance()) // MIS EN COMM. LE 17/02 POUR TEST : RETIRER COMMENTAIRE APRES TEST
+      tap(() => this.screenDisappearance())
     ).subscribe();
   }
 
